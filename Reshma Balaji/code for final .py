04fb6012c0372674292a72c7123f0ef1b8fcf0e7
reshmabalaji <221427@c1mrxbjhh3qd.isd720.com>
2017-05-25 09:59:28 -0500

    Code for final - 1

---
+++ Reshma Balaji/code for final .py
@@ -0,0 +1,24 @@
+import random
+
+
+x = int(raw_input("Please enter a number up to 100. "))
+z = random.randint(40,100)
+""" x and z can both be used below without errors -- testing/learning"""
+
+randomNumber = random.randint(1,z)
+print "first random",randomNumber
+
+
+if 35 >= randomNumber:
+    newRandom = randomNumber * .5
+    newRandom = newRandom + 9
+    print "second random " "%.f" % newRandom
+
+elif 36 <= randomNumber <= 93:
+    newRandom = randomNumber + 7
+    print "second random " "%.f" % newRandom
+
+else:
+    newRandom = randomNumber
+    print "second random",newRandom
+    
